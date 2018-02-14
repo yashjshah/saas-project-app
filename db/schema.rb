15bac12aa40bb04596db29a379a161578b68b8a2
yashjshah <yashjshah@yahoo.com>
2018-02-14 21:18:50 +0000

    Complete Tenants and Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214173432) do
+ActiveRecord::Schema.define(version: 20180214201236) do
 
   create_table "members", force: :cascade do |t|
     t.integer  "tenant_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20180214173432) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "plan"
   end
 
   add_index "tenants", ["name"], name: "index_tenants_on_name"
